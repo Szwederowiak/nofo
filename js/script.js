da8e4223963aeb63bb38636e24ab4367ce303c63
Szwederowiak <35521057+Szwederowiak@users.noreply.github.com>
2020-08-25 19:27:57 +0200

    y

--- js/script.js
+++ js/script.js
@@ -4,3 +4,15 @@ const mainNav = document.getElementById('main-nav');
 mainNavToggleBtn.addEventListener('click', () => {
     mainNav.classList.toggle('active');
 });
+
+// Bottom padding;
+
+const footer = document.getElementById('site-footer');
+
+document.body.style.paddingBottom = footer.getBoundingClientRect().height + 'px';
+
+window.addEventListener('resize', () => {
+    document.body.style.paddingBottom = footer.getBoundingClientRect().height + 'px';
+})
+
+
